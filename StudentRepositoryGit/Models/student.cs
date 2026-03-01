1ed9596fca10fc6258eca0c11b5960cf30445232
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-03-01 02:08:33 +0100

    Model_class Added

---
+++ StudentRepositoryGit/Models/student.cs
@@ -0,0 +1,9 @@
+﻿namespace StudentRepositoryGit.Models
+{
+    public class student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }= string.Empty;
+        public int BirthYear { get; set; }
+    }
+}
