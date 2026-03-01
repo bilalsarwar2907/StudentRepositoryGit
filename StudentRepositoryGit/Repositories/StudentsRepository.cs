62080f61a82f55936958540386dce5543d888a97
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-03-01 06:03:33 +0100

    Deleted_Repo

---
+++ StudentRepositoryGit/Repositories/StudentsRepository.cs
@@ -0,0 +1,12 @@
+﻿using StudentRepositoryGit.Models;
+namespace StudentRepositoryGit.Repositories
+{
+    public class StudentsRepository
+    {
+        private readonly List<Student> _students = new List<Student>();
+
+        private int _nextId = 1;
+
+      
+    }
+}
