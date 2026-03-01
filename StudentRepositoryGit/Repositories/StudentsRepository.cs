4b17b4d525a85f5801ef18bc18f00bd554a4c952
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-03-01 07:15:43 +0100

    Add all CRUD methods to StudentsRepository

--- StudentRepositoryGit/Repositories/StudentsRepository.cs
+++ StudentRepositoryGit/Repositories/StudentsRepository.cs
@@ -11,6 +11,46 @@ namespace StudentRepositoryGit.Repositories
         {
 
         }
-      
+        public IEnumerable<Student> Get()
+
+        {
+            return _students;
+
+        }
+        public Student? GetById(int id)
+        {
+            return _students.FirstOrDefault(student => student.Id == id);
+        }
+        public Student? Add(Student student)
+        {
+            if (student == null)
+
+                return null;
+            student.Id = _nextId++;
+            _students.Add(student);
+            return student;
+
+        }
+        public Student? Update(int id, Student data)
+        {
+           var student = GetById(id);
+            if (student == null)
+                return null;
+            student.Name = data.Name;
+            student.BirthYear = data.BirthYear;
+
+            return student;
+
+        }
+
+
+        public Student? Delete(int id)
+        {
+           var student = GetById(id);
+            if (student == null)
+                return null;
+            _students.Remove(student);
+            return student;
+        }
     }
 }
